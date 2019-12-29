f2f342b93e19d44ae2420d3089b9064b9b32a1bd
raineer <delaritaraineer81@gmail.com>
2019-12-29 22:18:30 +0800

    pages

--- src/app/posts/posts.module.ts
+++ src/app/posts/posts.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { PostsRoutingModule } from "./posts-routing.module";
 import { SharedModule } from "../shared/shared.module";
-import { PostListComponent } from "./pages/post-list/post-list.component";
+import { PostListComponent } from "./pages/";
 import { PostsComponent } from "./posts.component";
 
 @NgModule({
